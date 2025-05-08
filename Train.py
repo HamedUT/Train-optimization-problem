4fdc3fc6387aaef235cbfc165475d45d895c0ef1
HamedUT <h.darbandi@utwente.nl>
2025-05-08 11:19:43 +0200

    v15.1: organizing

--- Train.py
+++ Train.py
@@ -78,7 +78,7 @@ def Initializer(delta_s, max_acc, max_braking, data, m, C_d, A, C, eta, WindSpee
         sys.exit()
     return v_opt, P_opt
 
-def train(distance_remaining, delta_s, max_acc, max_braking, max_p, data, m, C_d, A, C, eta, braking_eff, time_remaining, WindSpeed, v_init, max_p_sub1, max_p_sub2, mu_curve, rho, V0):
+def train(distance_remaining, delta_s, max_acc, max_braking, max_p, data, m, C_d, A, C, eta, braking_eff, time_remaining, WindSpeed, v_init, max_p_sub1, max_p_sub2, mu_curve, Consider_electrical_losses, rho, V0):
     start_train = time.time()
     v_opt, P_opt = Initializer(delta_s, max_acc, max_braking, data, m, C_d, A, C, eta, WindSpeed, v_init)
     D = data.keys()
@@ -124,7 +124,7 @@ def train(distance_remaining, delta_s, max_acc, max_braking, max_p, data, m, C_d
             m * 9.807 * data[d]['grade'] +
             m * (model.v[d] - model.v[prev_d]) / (2 * delta_s / (model.v[d] + model.v[prev_d])) +
             mu_curve * m * (model.v[d]**2) / data[d]['radius']
-            ) + rho * distance_remaining * 0 * ( #electrical losses
+            ) + rho * distance_remaining * 1 * ( #electrical losses
             (
                 model.v[d] / eta * (
                 0.5 * 1.225 * C_d * A * (model.v[d] + WindSpeed)**2 +
@@ -484,26 +484,27 @@ def process_speed_limits(filepath, delta_s, d_init):
 
 # Electrical Parameters
 rho, V0 = 0.00003, 1500 # Ohms/m, Voltage (V)
+Consider_electrical_losses = 1 # Electrical losses in Train function (0: do not consider, 1: consider)
 
 # Train Parameters (SNG)
 Rotatory_inertia_factor = 0.0674
 m = 152743 * (1 + Rotatory_inertia_factor) # kg (train weight, but should be variable later)
 A, C, C_d = 2.88*4.25, 0.002, 0.8 # m^2 (Frontal area), (Rolling resistance coefficient), (Drag coefficient)
-eta, braking_eff = 0.857, 0.1 # Efficiency of the train's propulsion system, Regenerative braking efficiency
+eta = 0.857  # Efficiency of the train's propulsion system
+braking_eff = 0.0  # Regenerative braking efficiency
 max_v, max_acc, max_braking = 44.444, 0.81, 0.5 # m/s = 160 km/h (max velocity), m/s2 (max acceleration), (max braking)
-Auxiliary_power = 65558 #  Average auxiliary power consumption (W)
-max_p = 1393000 - Auxiliary_power # W (max power)
+max_p = 1393000 # W (max power)
 mu_curve = 0.001 # Curve resistance coefficient (m/s^2) (assumed value, can be adjusted based on specific conditions)
 
 # Distance discretization
-total_time = 7 * 60 # (sec) From Substation 1 to Substation 2
+total_time = 431 # (sec) From Substation 1 to Substation 2
 delta_s = 200  # Distance step in meters
 
 # Time-dependent parameters
 max_p_sub1 = 2.0 * 1000000 # W (max power for substation 1)
 max_p_sub2 = 2.0 * 1000000 # W (max power for substation 2)
 # WindSpeed = random.choice([random.uniform(-5, -2), random.uniform(2, 5)])  # m/s (Wind speed, excluding -2 to 2)
-WindSpeed = 2.5
+WindSpeed = 0
 
 # Initialize initial conditions
 v_init = 0 / 3.6 # (Enter in km/h) Initial velocity (m/s)
@@ -514,10 +515,12 @@ time_remaining = total_time - t_init # Remaining time (s), Remaining distance (m
 speed_limit_csv_path = r"c:\Users\DarbandiH\OneDrive - University of Twente\Postdoc\Python\Train optimization problem\mrsp#2839#20250507.csv"
 speed_limits_dict, speed_limit_array, distance_remaining = process_speed_limits(speed_limit_csv_path, delta_s, d_init)
 print(f"Distance remaining: {distance_remaining} m")
-gradients = generate_random_gradients(distance_remaining, delta_s, mode = "const", max_gradient=0.0015)
-track_radius = generate_random_track_radius(distance_remaining, delta_s, "const", min_radius=1000, max_radius=1e6)
+
+gradients = generate_random_gradients(distance_remaining, delta_s, mode = "const", max_gradient=0.0)
+track_radius = generate_random_track_radius(distance_remaining, delta_s, "const", min_radius=1000, max_radius=1e9)
 
 data = {0: {'grade': gradients[0], 'radius': track_radius[0], 'speed_limit': speed_limit_array[0]}}
+
 for i in range(1, int(distance_remaining / delta_s) + 1):
     data[i * delta_s] = {
         'grade': gradients[min(i, len(gradients) - 1)],
@@ -525,7 +528,7 @@ for i in range(1, int(distance_remaining / delta_s) + 1):
         'speed_limit': speed_limit_array[min(i, len(speed_limit_array) - 1)]
     }
   
-model, termination_condition = train(distance_remaining, delta_s, max_acc, max_braking, max_p, data, m, C_d, A, C, eta, braking_eff, time_remaining, WindSpeed, v_init, max_p_sub1, max_p_sub2, mu_curve, rho, V0)
+model, termination_condition = train(distance_remaining, delta_s, max_acc, max_braking, max_p, data, m, C_d, A, C, eta, braking_eff, time_remaining, WindSpeed, v_init, max_p_sub1, max_p_sub2, mu_curve, Consider_electrical_losses, rho, V0)
 end_time = time.time()
 
 if termination_condition in [pyomo.TerminationCondition.optimal, pyomo.TerminationCondition.locallyOptimal]:
